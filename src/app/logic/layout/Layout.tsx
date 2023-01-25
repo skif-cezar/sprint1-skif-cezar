434ad47cfb334a068271dbaa8f528c1f60a0c61f
skif-cezar <skif.cezar@gmail.com>
2023-01-25 20:42:01 +0300

    feat: add layout component

--- src/app/logic/layout/Layout.tsx
+++ src/app/logic/layout/Layout.tsx
@@ -1,6 +1,24 @@
 import React from 'react';
+import clsx from 'clsx';
+import styles from 'src/app/logic/layout/Layout.module.scss';
 
 /**
  * Layout component
  */
-export const Layout: React.FC = () => <div>HEllo</div>;
+export const Layout: React.FC = () => {
+  const WRAPPER_STYLES = clsx(styles.wrapper);
+  const MAIN_STYLES = clsx(styles.main);
+
+  return (
+    <div className={WRAPPER_STYLES}>
+      <header>Header</header>
+      <aside>
+         <nav>Navigation</nav>
+      </aside>
+      <main className={MAIN_STYLES}>
+        <section>Section</section>
+      </main>
+      <footer>Footer</footer>
+    </div>
+  );
+};
