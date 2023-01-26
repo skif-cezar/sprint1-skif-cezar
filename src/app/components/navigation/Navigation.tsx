5b9287d825c594639f108dac53da622599482fa5
skif-cezar <skif.cezar@gmail.com>
2023-01-26 21:42:26 +0300

    feat: add view buttons

--- src/app/components/navigation/Navigation.tsx
+++ src/app/components/navigation/Navigation.tsx
@@ -10,11 +10,15 @@ import styles from 'src/app/components/navigation/Navigation.module.scss';
  */
 export const Navigation: React.FC = () => {
   const NAVIGATION_STYLES = clsx(styles.navigation);
+  const BUTTON_TILE_STYLES = clsx(styles.button_tile);
+  const BUTTON_LIST_STYLES = clsx(styles.button_list);
 
   return (
     <div className={NAVIGATION_STYLES}>
       <Search />
       <FilterButton />
+      <button className={BUTTON_TILE_STYLES} type='button' aria-label='Плитка' />
+      <button className={BUTTON_LIST_STYLES} type='button' aria-label='Список' />
     </div>
   );
 };
