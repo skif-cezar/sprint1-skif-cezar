726511e65af25428c01355ef3812ec05f1f78522
skif-cezar <skif.cezar@gmail.com>
2023-01-31 17:43:33 +0300

    feat: add responsive window view to 768px

--- src/app/components/header/Header.tsx
+++ src/app/components/header/Header.tsx
@@ -12,6 +12,7 @@ import styles from 'src/app/components/header/Header.module.scss';
 export const Header: React.FC = () => {
   const HEADER_STYLES = clsx(styles.header);
   const LOGO_STYLES = clsx(styles.logo);
+  const BURGER_MENU_STYLES = clsx(styles.burger_menu);
   const TITLE_STYLES = clsx(styles.title);
   const AUTHORIZATION_STYLES = clsx(styles.authorization);
   const WELCOME_STYLES = clsx(styles.welcome);
@@ -22,6 +23,7 @@ export const Header: React.FC = () => {
       <NavLink to='/' className={LOGO_STYLES}>
         <img src={logoSrc} alt='Логотип сайта Cleverland' width={165} height={40} />
       </NavLink>
+      <button className={BURGER_MENU_STYLES} type='button'></button>
       <h1 className={TITLE_STYLES}>Библиотека</h1>
       <div className={AUTHORIZATION_STYLES}>
         <span className={WELCOME_STYLES}>Привет, Иван!</span>
